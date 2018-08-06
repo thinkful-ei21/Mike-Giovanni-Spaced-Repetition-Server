8a3c17f43153166ecfa24dbd10edd8fde4370262
Michael Le-Reiver <m.lereiver@gmail.com>
2018-08-06 09:48:10 -0700

    hooked up user schema, routes, and modified config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
+  MONGODB_URI:
         process.env.DATABASE_URL || 'mongodb://admin:a09fds87@ds113942.mlab.com:13942/spaced-repetition',
-  TEST_DATABASE_URL:
+  TEST_MONGODB_URI:
         process.env.TEST_DATABASE_URL ||  'mongodb://localhost/thinkful-backend-test',
   JWT_SECRET : process.env.JWT_SECRET,
   JWT_EXPIRY : process.env.JWT_EXPIRY || '7d'
--- routes/user-router.js
+++ routes/user-router.js
@@ -1,56 +1,133 @@
 'use strict';
-const { Strategy: LocalStrategy } = require('passport-local');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// Assigns the Strategy export to the name JwtStrategy using object destructuring
-// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Assigning_to_new_variable_names
-const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const {User} = require('../models/user-model');
 
-const { User } = require('../models/user-model');
-const { JWT_SECRET } = require('../config');
+const router = express.Router();
 
-const localStrategy = new LocalStrategy((username, password, callback) => {
-  let user;
-  User.findOne({ username: username })
-    .then(_user => {
-      user = _user;
-      if (!user) {
-        // Return a rejected promise so we break out of the chain of .thens.
-        // Any errors like this will be handled in the catch block.
+const jsonParser = bodyParser.json();
+
+// Post to register a new user
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  if (missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    });
+  }
+
+  const stringFields = ['username', 'password'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+
+  // If the username and password aren't trimmed we give an error.  Users might
+  // expect that these will work without trimming (i.e. they want the password
+  // "foobar ", including the space at the end).  We need to reject such values
+  // explicitly so the users know what's happening, rather than silently
+  // trimming them and expecting the user to understand.
+  // We'll silently trim the other fields, because they aren't credentials used
+  // to log in, so it's less of a problem.
+  const explicityTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 6,
+      // bcrypt truncates after 72 characters, so let's not give the illusion
+      // of security by storing extra (unused) info
+      max: 72
+    }
+  };
+  const tooSmallField = Object.keys(sizedFields).find(
+    field =>
+      'min' in sizedFields[field] &&
+            req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+            req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField]
+          .min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField]
+          .max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  let {username, password } = req.body;
+  // Username and password come in pre-trimmed, otherwise we throw an error
+  // before this
+
+  return User.find({username})
+    .count()
+    .then(count => {
+      if (count > 0) {
+        // There is an existing user with the same username
         return Promise.reject({
-          reason: 'LoginError',
-          message: 'Incorrect username or password'
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken',
+          location: 'username'
         });
       }
-      return user.validatePassword(password);
+      // If there is no existing user, hash the password
+      return User.hashPassword(password);
     })
-    .then(isValid => {
-      if (!isValid) {
-        return Promise.reject({
-          reason: 'LoginError',
-          message: 'Incorrect username or password'
-        });
-      }
-      return callback(null, user);
+    .then(hash => {
+      return User.create({
+        username,
+        password: hash
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.serialize());
     })
     .catch(err => {
-      if (err.reason === 'LoginError') {
-        return callback(null, false, err);
+      // Forward validation errors on to the client, otherwise give a 500
+      // error because something unexpected has happened
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
       }
-      return callback(err, false);
+      res.status(500).json({code: 500, message: 'Internal server error'});
     });
 });
 
-const jwtStrategy = new JwtStrategy(
-  {
-    secretOrKey: JWT_SECRET,
-    // Look for the JWT as a Bearer auth header
-    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
-    // Only allow HS256 tokens - the same as the ones we issue
-    algorithms: ['HS256']
-  },
-  (payload, done) => {
-    done(null, payload.user);
-  }
-);
-
-module.exports = { localStrategy, jwtStrategy };
+module.exports = router;
