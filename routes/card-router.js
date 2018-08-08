8e603133c48d677361613e06f90fb863dc8a0861
giovanni <mormagli@gmail.com>
2018-08-08 14:46:10 -0500

    added asynch populate method, changed insertafter to return a promise

--- routes/card-router.js
+++ routes/card-router.js
@@ -37,55 +37,58 @@ const insertAfter =(card, previous = 'null') => {
   //leaving previous null will insert at head of list
 
   card.previous = previous;
+  return new Promise((resolve) => {
+    if(card.previous !== 'null'){
+      return Card.findOne({_id:card.previous})
+        .then(prev =>{
+          card.next = prev.next;
+          resolve(Card.create(card));    
+        })
+        .then(newCard =>{
+          if(newCard.next !== 'null'){
+            return Card.findOneAndUpdate({_id:card.next}, {previous:`${newCard._id}`})
+              .then(()=>{
+                return Card.findOneAndUpdate({_id:card.previous}, {next:`${newCard._id}`})
+                  .then(()=>{
+                    resolve(newCard);
+                  });
+              });
+          }
+          else{
+            return Card.findOneAndUpdate({_id:card.previous}, {next:`${newCard._id}`})
+              .then(()=>{
+                resolve(newCard);
+              });
+          }
+        });
+    }
+    else if (card.previous === 'null'){
+      console.log(card);
+      //if we want to insert at the head of our ll, we will insert -before- our first element
+      return Card.find({user_id: card.user_id, previous: 'null'})
+        .then(found =>{
+          console.log(found);
+          if(found.length === 0){
+            resolve(Card.create(card));
+          }
+          else if(found.length >0){
+            console.log('inserting first');
+            card.next = found[0]._id;
+            return Card.create(card)
+              //create card has to happen after we find our first element(to get it's _id),
+              // but before we update that element's previous (because we need to create in order to have a new _id)
+              .then((newCard) =>{
+                return Card.findOneAndUpdate({_id:found[0]._id}, {previous:`${newCard._id}`})
+                  .then(()=>{
+                    //but we don't actually care about what our update returns, we want to return our new card
+                    resolve(newCard);
+                  });
+              });
+          }
 
-  if(card.previous !== 'null'){
-    return Card.findOne({_id:card.previous})
-      .then(prev =>{
-        card.next = prev.next;
-        return Card.create(card);    
-      })
-      .then(newCard =>{
-        if(newCard.next !== 'null'){
-          return Card.findOneAndUpdate({_id:card.next}, {previous:`${newCard._id}`})
-            .then(()=>{
-              return Card.findOneAndUpdate({_id:card.previous}, {next:`${newCard._id}`})
-                .then(()=>{
-                  return newCard;
-                });
-            });
-        }
-        else{
-          return Card.findOneAndUpdate({_id:card.previous}, {next:`${newCard._id}`})
-            .then(()=>{
-              return newCard;
-            });
-        }
-      });
-  }
-  else if (card.previous === 'null'){
-    //if we want to insert at the head of our ll, we will insert -before- our first element
-    return Card.find({user_id: card.user_id, previous: 'null'})
-      .then(found =>{
-        if(found.length ===0){
-          return Card.create(card);
-        }
-        else if(found.length >0){
-          card.next = found[0]._id;
-          return Card.create(card)
-          //create card has to happen after we find our first element(to get it's _id),
-          // but before we update that element's previous (because we need to create in order to have a new _id)
-            .then((newCard) =>{
-              return Card.findOneAndUpdate({_id:found[0]._id}, {previous:`${newCard._id}`})
-                .then(()=>{
-                  //but we don't actually care about what our update returns, we want to return our new card
-                  return newCard;
-                });
-            });
-        }
-
-      });
-  }
-
+        });
+    }
+  });
 };
 
 const insertAt =(card, index) => {
@@ -113,7 +116,57 @@ const insertAt =(card, index) => {
 };
 
 
+const populateCards = (_id) =>{
 
+  let index = 0;
+  console.log('here?')
+  async function iterate (cardArr, index) {
+    
+    if(index === cardArr.length){
+      return new Promise((resolve)=>{
+        resolve()
+      })
+
+    }
+
+    console.log('here again')
+    if(index === cardArr.length){
+      return;
+    }
+    const cardTemplate = cardArr[index];
+    cardTemplate.user_id = _id;
+
+    let done = await insertAfter(cardTemplate)
+    return (iterate(cardArr, index +1));
+  }
+
+  return iterate(cardArr, index);
+
+  // };
+  // console.log('here')
+
+  // for (let index = 0; index < cardArr.length;) {
+  //   const cardTemplate = cardArr[index];
+  //   cardTemplate.user_id = _id;
+  //   insertAfter(cardTemplate)
+  //     .then(card =>{
+  //       console.log(card);
+  //       index++;});
+    
+  // }
+
+  // for (let cardTemplate of cardArr){
+  //   cardTemplate.user_id = _id;
+
+  //   let obj = insertAfter(cardTemplate)
+
+
+
+
+
+  // return;
+
+};
 
 
 router.post('/', jsonParser, (req, res) => {
@@ -182,10 +235,15 @@ router.get('/', jsonParser, (req, res) => {
 // look at card database and only returns first item in LL thats associated with user 
   const _id = req.user._id;
 
-  Card.findOne({user_id: _id, previous: 'null'})
-    .then(card => {
-      // console.log(card);
-      return card;
+  Card.find({user_id: _id, previous: 'null'})
+    .then(cards => {
+      if(cards.length > 0){
+        return cards[0];
+      }
+      else{
+        return populateCards(_id)
+          .then(()=>Card.findOne({user_id: _id, previous: 'null'}));
+      }
     })
     .then(card => {
       // console.log(card)
