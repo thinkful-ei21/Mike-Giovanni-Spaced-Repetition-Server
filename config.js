67b7b2b7ec56e6b4b2a86b4c65569f1f5266ca13
Michael Le-Reiver <m.lereiver@gmail.com>
2018-08-09 12:12:03 -0700

    added tests for user auth, login, register

--- config.js
+++ config.js
@@ -6,12 +6,7 @@ module.exports = {
   MONGODB_URI:
         process.env.DATABASE_URL || 'mongodb://admin:a09fds87@ds113942.mlab.com:13942/spaced-repetition',
   TEST_MONGODB_URI:
-        process.env.TEST_DATABASE_URL ||  'mongodb://localhost/thinkful-backend-test',
+        process.env.TEST_DATABASE_URL ||  'mongodb://localhost/spaced-rep-test',
   JWT_SECRET : process.env.JWT_SECRET,
   JWT_EXPIRY : process.env.JWT_EXPIRY || '7d'
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
-  // TEST_DATABASE_URL:
-  //     process.env.TEST_DATABASE_URL ||
-  //     'postgres://localhost/thinkful-backend-test'
 };
--- test/setup.test.js
+++ test/setup.test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
