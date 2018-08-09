d5c2bb305c248d8b57428aec0ac39731d8570c97
giovanni <mormagli@gmail.com>
2018-08-08 19:44:30 -0500

    fixed infuriating insertAfter bug, where old object ID was being maintained and passed in as prop for new obj creation. working now(though in retrospect it is a bit inefficient

---
+++ db/utils.js
@@ -0,0 +1,159 @@
+'use strict';
+
+const {Card} = require('../models/card-model');
+
+
+
+const popCard =(user_id)=>{
+  //removes and returns first document in ll
+   
+  return Card.findOne({user_id:user_id, previous: 'null'})
+    .then(found => {
+  
+      return Card.findOneAndRemove({_id:found._id})
+        .then(()=>{
+          // console.log('removing', found, 'should update', found.next)
+          return Card.findOneAndUpdate({_id:found.next},{previous:'null'});
+        })
+        .then((updated)=>{
+          // console.log('updated?',updated)
+          return found;});
+    })
+    .catch(err=>{console.log('promise error: ', err);});
+};
+
+
+const insertAfter =(cardTemplate, previous = 'null') => {
+  //will insert a card after 'previous', chaning the relevant next and previous properties of adjacent cards
+  //leaving previous null will insert at head of list
+  // console.log('inserting', cardTemplate, 'after', previous)
+  
+  let card = {};
+  card.user_id = cardTemplate.user_id;
+  card.imageUrls =cardTemplate.imageUrls ;
+  card.previous = previous;
+  card.answer = cardTemplate.answer;
+  card.mValue = cardTemplate.mValue;
+  // console.log(card._id)
+  // if(card._id){delete card["_id"]}
+    
+  // console.log('inserting', card, 'after', previous)
+    
+  
+  return new Promise((resolve, reject) => {
+    if(card.previous !== 'null'){
+      return Card.findOne({_id:card.previous})
+        .then(prev =>{
+          // console.log('here!!!', card,prev)
+          card.next = prev.next;
+          return(Card.create(card));    
+        })
+        .then(newCard =>{
+          console.log('or here?',newCard);
+          if(newCard.next !== 'null'){
+             
+            return Card.findOneAndUpdate({_id:card.next}, {previous:`${newCard._id}`})
+              .then(()=>{
+                return Card.findOneAndUpdate({_id:card.previous}, {next:`${newCard._id}`})
+                  .then(()=>{
+                    resolve(newCard);
+                  });
+              });
+          }
+          else{
+            return Card.findOneAndUpdate({_id:card.previous}, {next:`${newCard._id}`})
+              .then(()=>{
+                resolve(newCard);
+              });
+          }
+        })
+        .catch(err =>{  reject(new Error(err)); });
+    }
+    else if (card.previous === 'null'){
+      // console.log(card);
+      //if we want to insert at the head of our ll, we will insert -before- our first element
+      return Card.find({user_id: card.user_id, previous: 'null'})
+        .then(found =>{
+          // console.log(found);
+          if(found.length === 0){
+            resolve(Card.create(card));
+          }
+          else if(found.length >0){
+            console.log('inserting first');
+            card.next = found[0]._id;
+            return Card.create(card)
+            //create card has to happen after we find our first element(to get it's _id),
+            // but before we update that element's previous (because we need to create in order to have a new _id)
+              .then((newCard) =>{
+                return Card.findOneAndUpdate({_id:found[0]._id}, {previous:`${newCard._id}`})
+                  .then(()=>{
+                    //but we don't actually care about what our update returns, we want to return our new card
+                    resolve(newCard);
+                  });
+              });
+          }
+  
+        })
+        .catch(err =>{  reject(new Error(err)); });
+    }
+  });
+};
+
+
+
+
+
+const insertAt =(card, index) => {
+  //loop thorough our ll ( a while loop, prob.) to find the card before our index, then call insertAfter()
+  let i = 0;
+      
+  console.log('inserting:', card);
+  const findNext = (prevID = 'null') => {
+    console.log(i);
+    Card.findOne({user_id: card.user_id, previous: prevID})
+      .then(found=>{
+        // console.log('found next:',found)
+        i++;
+        if(i<index && found.next !== 'null'){
+          return findNext(found.next);
+        }
+        else{
+          console.log('inserting after', found);
+          return insertAfter(card, found._id);}
+      });  
+  };
+  return   findNext();
+      
+};
+
+const populateCards = (_id) =>{
+
+    let index = 0;
+    console.log('here?')
+    async function iterate (cardArr, index) {
+      
+      if(index === cardArr.length){
+        return new Promise((resolve)=>{
+          resolve()
+        })
+      }
+  
+      console.log('here again', index)
+      if(index === cardArr.length){
+        return;
+      }
+      const cardTemplate = cardArr[index];
+      cardTemplate.user_id = _id;
+  
+      let done = await insertAfter(cardTemplate)
+      return (iterate(cardArr, index +1));
+    }
+  
+    return iterate(cardArr, index);
+  
+  };
+
+
+
+
+module.exports = {populateCards, insertAfter, insertAt, popCard};
--- routes/card-router.js
+++ routes/card-router.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {cardArr} = require('../cardDB.js');
+const {cardArr} = require('../db/cardDB');
 const {Card} = require('../models/card-model');
+const {popCard, insertAfter, insertAt, populateCards} = require('../db/utils');
 
 const router = express.Router();
 
@@ -11,136 +12,9 @@ const jsonParser = bodyParser.json();
 
 
 
-const popCard =(user_id)=>{
-  //removes and returns first document in ll
- 
-  return Card.findOne({user_id:user_id, previous: 'null'})
-    .then(found => {
-
-      return Card.findOneAndRemove({_id:found._id})
-        .then(()=>{
-          Card.findOneAndUpdate({_id:found.next},{previous:'null'})
-        })
-        .then(()=>{return found;});
-    })
-    .catch(err=>{console.log('promise error: ', err);});
-};
-
-
-
-const insertAfter =(card, previous = 'null') => {
-  //will insert a card after 'previous', chaning the relevant next and previous properties of adjacent cards
-  //leaving previous null will insert at head of list
-
-  card.previous = previous;
-  return new Promise((resolve) => {
-    if(card.previous !== 'null'){
-      return Card.findOne({_id:card.previous})
-        .then(prev =>{
-          card.next = prev.next;
-          resolve(Card.create(card));    
-        })
-        .then(newCard =>{
-          if(newCard.next !== 'null'){
-            return Card.findOneAndUpdate({_id:card.next}, {previous:`${newCard._id}`})
-              .then(()=>{
-                return Card.findOneAndUpdate({_id:card.previous}, {next:`${newCard._id}`})
-                  .then(()=>{
-                    resolve(newCard);
-                  });
-              });
-          }
-          else{
-            return Card.findOneAndUpdate({_id:card.previous}, {next:`${newCard._id}`})
-              .then(()=>{
-                resolve(newCard);
-              });
-          }
-        });
-    }
-    else if (card.previous === 'null'){
-      console.log(card);
-      //if we want to insert at the head of our ll, we will insert -before- our first element
-      return Card.find({user_id: card.user_id, previous: 'null'})
-        .then(found =>{
-          console.log(found);
-          if(found.length === 0){
-            resolve(Card.create(card));
-          }
-          else if(found.length >0){
-            console.log('inserting first');
-            card.next = found[0]._id;
-            return Card.create(card)
-              //create card has to happen after we find our first element(to get it's _id),
-              // but before we update that element's previous (because we need to create in order to have a new _id)
-              .then((newCard) =>{
-                return Card.findOneAndUpdate({_id:found[0]._id}, {previous:`${newCard._id}`})
-                  .then(()=>{
-                    //but we don't actually care about what our update returns, we want to return our new card
-                    resolve(newCard);
-                  });
-              });
-          }
-
-        });
-    }
-  });
-};
-
-const insertAt =(card, index) => {
-  //loop thorough our ll ( a while loop, prob.) to find the card before our index, then call insertAfter()
-
-  let i = 0;
-  let currCard = 'null';
-  let brk = false;
-
-  while(i < index && !brk){
-
-    Card.findOne({user_id: card.user_id, previous: currCard})
-      .then(cc =>{
-        currCard = cc;
-        if(cc.next !== 'null'){
-          i++;
-        }
-        else{
-          brk = true;
-        }
-      });
-  }
-
-  return insertAfter(card, currCard);
-};
-
-
-const populateCards = (_id) =>{
-
-  let index = 0;
-  console.log('here?')
-  async function iterate (cardArr, index) {
-    
-    if(index === cardArr.length){
-      return new Promise((resolve)=>{
-        resolve()
-      })
-    }
-
-    console.log('here again', index)
-    if(index === cardArr.length){
-      return;
-    }
-    const cardTemplate = cardArr[index];
-    cardTemplate.user_id = _id;
-
-    let done = await insertAfter(cardTemplate)
-    return (iterate(cardArr, index +1));
-  }
-
-  return iterate(cardArr, index);
-
-};
-
-
 router.post('/', jsonParser, (req, res) => {
+
+  //post endpoint is depreciated, I think.... leaving it in for now in case it helps with testing
 
   //this endpoint currently appends a new card to the end of our logged-in user's linked list.
   //that's probably not what this endpoint will do in the end, 
@@ -177,8 +51,6 @@ router.put('/', jsonParser, (req, res) =>{
   let result = false;
   let mValue;
 
-
-
   // gets answer from client, compares with first element of user's linked list
   // will update linked list with new values
   // responds with resultAnswer, result (true/false)
@@ -186,31 +58,32 @@ router.put('/', jsonParser, (req, res) =>{
     .then(card => {
       if(!card) {
         const DBError = new Error ('Database Error');
-        return Promise.reject(DBError)
+        return Promise.reject(DBError);
       } else {
         if(card.answer.toLowerCase() === answer) {
-          console.log('answers ', answer, card.answer)
+          console.log('answers ', answer, card.answer);
           result = true;
           mValue = card.mValue * 2;
         } else {
           result = false;
           mValue = 1;
         }
-        return card._id
+        return card._id;
       }
     })
     .then(() => {
       let card = popCard(_id);
       return card.then(card => {
         card.mValue = mValue;
-        console.log('value', card)
+        // console.log('value', card)
         return insertAt(card, mValue);
-      })
+      });
     })
-    .then(res => {
-      res.status(201).json({answer, result})
-    })
-})
+    .then(() => {
+      res.status(201).json({answer, result});
+    });
+});
+
 
 router.get('/', jsonParser, (req, res) => {
 // look at card database and only returns first item in LL thats associated with user 
@@ -235,6 +108,7 @@ router.get('/', jsonParser, (req, res) => {
       if (err.reason === 'ValidationError') {
         return res.status(err.code).json(err);
       }
+      else res.status(400).json(err);
     });
 });
 
